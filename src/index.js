16477c1cf0451246d79d87a4dd1af5f3b7b9f97b
leivadiazjulio@gmail.com <leivadiazjulio@gmail.com>
2020-01-06 12:48:33 +0100

    connecting redux with App

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
 import 'font-awesome/css/font-awesome.css'
 
-let WithRouter = () => <BrowserRouter><App /></BrowserRouter>
+import { Provider } from 'react-redux'
+import generateStore from './redux/store'
 
-ReactDOM.render(<WithRouter />, document.getElementById('root'));
+let store = generateStore();
+
+let WithRouter = () => <BrowserRouter><App /></BrowserRouter>
+let WithStore = () => <Provider store={store}><WithRouter/></Provider>
+
+ReactDOM.render(<WithStore />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
